bbf7bf1717ea6dbd69ed654d52f4e5e65ad9d7e6
how_long_will_it_go <33710058+bach001@users.noreply.github.com>
2023-03-31 02:33:42 +0800

    Update ReverseTextBlock.py

--- utils/ReverseTextBlock.py
+++ utils/ReverseTextBlock.py
@@ -1,25 +1,30 @@
+"""
+reversing file blocks tagged with number
+"""
+
 import re
 import os
 
+
 class ReverseTextBlock:
 
-    def __init__(self, path):
-        self.prefix = os.path.dirname(path)
-        self.path = path
+    def __init__(self, file_path):
+        self.prefix = os.path.dirname(file_path)
+        self.path = file_path
 
     def reverse(self):
         p = re.compile(r'^\s*[0-9]+\s*\n$')
         with open(self.path) as f:
             keys = []
             vals = []
-            blank_lines = ''
+            head_lines = ''
             lines = ''
             matched = False
             for line in f:
                 if p.fullmatch(line):
                     keys.append(line)
                     if not matched:
-                        blank_lines = lines
+                        head_lines = lines
                     else:
                         vals.append(lines)
                     matched = True
@@ -30,17 +35,19 @@ class ReverseTextBlock:
 
         print(len(keys))
         print(len(vals))
-        #keys.reverse()
-        #vals.reverse()
-        
-        output_path = os.path.join(self.prefix, 'output.txt')
-        with open(output_path, 'w') as f:
-            f.write(blank_lines)
+        # keys.reverse()
+        # vals.reverse()
+
+        # output = 'output.txt'
+        # output = os.path.join(self.prefix, 'output.txt')
+        with open("./output.txt", 'w') as f:
+            f.write(head_lines)
             for i in reversed(range(len(keys))):
                 f.write(keys[i])
                 f.write(vals[i])
 
 
 if __name__ == '__main__':
-    obj = ReverseTextBlock('/home/bach/Documents/1111.txt')
+    path = '/home/bach/Documents/test.txt'
+    obj = ReverseTextBlock(path)
     obj.reverse()
